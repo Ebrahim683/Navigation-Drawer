5c5e7548c8838439b8b49b9d8f3b1752f4479943
Mazharul Sabbir <42198761+mazharulsabbir@users.noreply.github.com>
2021-12-28 13:40:21 +0600

    Update MainActivity.kt

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.navigationdrawer
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +20,16 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
-        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawer_id, R.string.open, R.string.close)
-        drawer_id.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            this, drawer_id, R.string.open, R.string.close
+        )
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_id)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
+
         nav_id.setNavigationItemSelectedListener { item ->
+            Log.d(TAG, "onCreate: menu item selected!")
             when (item.itemId) {
                 R.id.m_setting -> {
                     tv.text = "Setting"
@@ -27,10 +37,16 @@ class MainActivity : AppCompatActivity() {
                 R.id.m_email -> {
                     tv.text = "Email"
                 }
+                else -> {
+                    tv.text = "Clicked! But id not matched!"
+                }
             }
+            item.isChecked = true
+            drawerLayout.closeDrawers()
             true
         }
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true
